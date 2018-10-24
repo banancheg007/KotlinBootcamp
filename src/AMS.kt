f97c6c3c480f2180b5bc53eadbb67843b17837b6
Ruslan <ruslansolodovnyk@gmail.com>
2018-10-24 22:08:13 +0300

    Compact Functions lesson 3.14

--- src/AMS.kt
+++ src/AMS.kt
@@ -2,13 +2,38 @@ import java.util.*
 
 fun main(args: Array<String>){
     feedTheFish()
+
+    var bubbles = 0
+    while (bubbles < 50) {
+        bubbles++
+    }
 }
 
+fun getDirtySensorReading() = 20
+
 fun shouldChangeWater(day: String, temperature: Int = 22, dirty: Int = 20): Boolean{
-    return true
+
+    val isTooHot = temperature >30
+    val isDirty = dirty > 30
+    val isSunday = day == "Sunday"
+
+    return when{
+        isTooHot(temperature) -> true
+        isDirty(dirty) -> true
+        isSunday(day) -> true
+
+        else -> false
+    }
 }
-fun shouldChangeWater2(temperature: Int = 22, dirty: Int = 20, day: String){
+
+fun makeNewAquarium() = println("Building a new aquarium.......")
+fun aquariumStatusReport (aquarium: Any  = makeNewAquarium()){
+
 }
+
+fun isTooHot(temperature: Int) = temperature > 30
+fun isDirty(dirty: Int) = dirty > 30
+fun isSunday(day: String) = day == "Sunday"
 
 
 
@@ -19,7 +44,6 @@ fun feedTheFish(){
     shouldChangeWater(day, 20, 50)
     shouldChangeWater(day)
     shouldChangeWater(day, dirty = 50)
-    shouldChangeWater2(day = "Monday")
     if (shouldChangeWater(day)){
         println("Change the water today")
     }
