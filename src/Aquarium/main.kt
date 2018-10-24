577d7d0efd0b86743402f3bf52bf37427142640f
Ruslan <ruslansolodovnyk@gmail.com>
2018-10-24 23:56:26 +0300

    Constructors lesson 4.4 practice

--- src/Aquarium/main.kt
+++ src/Aquarium/main.kt
@@ -11,4 +11,14 @@ internal fun buildAquarium(){
     println("Length: ${myAquarium.length}"+
             "Width: ${myAquarium.width}" +
             "Height: ${myAquarium.height}")
+
+    myAquarium.height = 80
+
+    println("Height: ${myAquarium.height} cm")
+    println("Volume: ${myAquarium.volume} liters")
+
+    val smallAquarium = Aquarium (20,15, 30)
+    println("Small aquarium: ${smallAquarium.volume} liters")
+
+    val myAquarium2 = Aquarium(numberOfFish = 9)
 }
