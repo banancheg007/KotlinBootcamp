c5a0c0834509fdc0865deb5487cd3d37fe4cf106
Ruslan <ruslansolodovnyk@gmail.com>
2018-10-25 00:01:18 +0300

    package Spice - Spice.kt SpiceMain.kt lesson 4.5 practice

---
+++ src/Spice/SpiceMain.kt
@@ -0,0 +1,18 @@
+package Spice
+
+fun main(args: Array<String>){
+    val spices1 = listOf(
+        Spice("curry", "mild"),
+        Spice("pepper", "medium"),
+        Spice("cayenne", "spicy"),
+        Spice("ginger", "mild"),
+        Spice("red curry", "medium"),
+        Spice("green curry", "mild"),
+        Spice("hot pepper", "extremely spicy")
+    )
+
+    val spice = Spice("cayenne", spiciness = "spicy")
+
+    val spicelist = spices1.filter {it.heat < 5}
+    fun makeSalt() = Spice("Salt")
+}
