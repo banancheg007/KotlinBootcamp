39fc12e756b4a1bec4ce6cd342b909c872500161
Ruslan <ruslansolodovnyk@gmail.com>
2018-10-24 22:38:04 +0300

    Lesson 3.16 Kotlin filters

--- src/AMS.kt
+++ src/AMS.kt
@@ -2,6 +2,7 @@ import java.util.*
 
 fun main(args: Array<String>){
     feedTheFish()
+    eagerExapmle()
 
     var bubbles = 0
     while (bubbles < 50) {
@@ -9,6 +10,26 @@ fun main(args: Array<String>){
     }
 }
 
+fun eagerExapmle(){
+    val decorations = listOf("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
+
+    val eager = decorations.filter { it[0] == 'p' }
+    println(eager)
+
+    //apply filter lazily
+    val filtered = decorations.asSequence().filter { it[0] == 'p' }
+    println(filtered)
+    println(filtered.toList())
+
+    //apply map lazily
+    val lazyMap = decorations.asSequence().map{
+        println("map: $it")
+         it
+    }
+    println(lazyMap)
+    println("first: ${lazyMap.first()}")
+
+}
 fun getDirtySensorReading() = 20
 
 fun shouldChangeWater(day: String, temperature: Int = 22, dirty: Int = 20): Boolean{
