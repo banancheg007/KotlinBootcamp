c273234e190362510310553abbd83583259aecf6
Ruslan <ruslansolodovnyk@gmail.com>
2018-10-24 17:36:31 +0300

    RandomDay lesson 3.6

--- src/AMS.kt
+++ src/AMS.kt
@@ -1,13 +1,16 @@
+import java.util.*
+
 fun main(args: Array<String>){
-    println("Hello, ${args[0]}!")
+    feedTheFish()
+}
 
-    val isUnit = println("This is an expression")
-    println(isUnit)
+fun feedTheFish(){
+    val day = randomDay()
+    val food = "pellets"
+    println("Today is $day and the fish eat $food")
+}
 
-    val temperature = 10
-    val isHot = if (temperature > 50) true else false
-    println(isHot)
-
-    val message = "You are ${if (temperature > 50) "fried" else "safe" } fish"
-    println(message)
+fun randomDay(): String{
+    val week = listOf("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")
+    return week[Random().nextInt(7)]
 }
