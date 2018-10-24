abab1f330fed297d4ab902edc18a5b3b33acae85
Ruslan <ruslansolodovnyk@gmail.com>
2018-10-24 22:26:17 +0300

    Lesson 3.15 practice using while

--- src/Fortune.kt
+++ src/Fortune.kt
@@ -1,12 +1,10 @@
 fun main(args: Array<String>) {
     var fortune: String = ""
-    repeat (10) {
+    while (!fortune.contains("Take it easy")) {
         fortune = getFortune(getBirthday())
         println("\nYour fortune is: $fortune")
-        if (fortune.contains("Take it easy")) return
     }
 }
-
 
 fun getBirthday(): Int {
     print("\nEnter your birthday: ")
