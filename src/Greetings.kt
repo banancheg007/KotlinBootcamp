bc0db73c3bfc92a960a72bfa0f84f601edcf6ceb
Ruslan <ruslansolodovnyk@gmail.com>
2018-10-24 17:27:10 +0300

    Greetings.kt with string template in 1 line

--- src/Greetings.kt
+++ src/Greetings.kt
@@ -1,7 +1,3 @@
 fun main(args: Array<String>){
-    if ( args[0].toInt() < 12){
-        println("Good morning, Kotlin")
-    }else{
-        println("Good night, Kotlin")
-    }
+    println("${if (args[0].toInt()<12) "Good morning, Kotlin" else  "Good night, Kotlin"}")
 }
