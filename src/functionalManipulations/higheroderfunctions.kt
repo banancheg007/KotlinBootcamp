afca31b793b7ea0eed0705eccdb0a233c14f9333
Ruslan <ruslansolodovnyk@gmail.com>
2018-11-06 23:19:45 +0200

    Add higher order functions let, run, apply

--- src/functionalManipulations/higheroderfunctions.kt
+++ src/functionalManipulations/higheroderfunctions.kt
@@ -1,12 +1,29 @@
 package functionalManipulations
 
+data class Fish(var name: String)
+
 fun main(args: Array<String>) {
-    var name: String = "ramiz"
-    name = myWith(name) {
+    var myName: String = "ramiz"
+    myName = myWith(myName) {
         capitalize()
     }
-    println(name)
+    println(myName)
+
+    //try run higher order function: returns result of execution of lambda
+    val fish = Fish("splashy")
+    println(fish.run { name })
+
+    //try apply: returns object on which it is applied, after applying the lambda
+    val fish2 = Fish("splashy").apply { name = "sharky" }
+    println(fish2.name)
+
+    //try let: returns the copy of the changed object
+    println(fish.let { it.name.capitalize() }
+        .let { it + " fish" }
+        .let { it.length}
+        .let { it + 31 })
 }
+
 fun myWith(name: String, block: String.() -> String): String {
     return name.block()
-} 
+}
