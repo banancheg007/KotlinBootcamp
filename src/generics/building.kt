cc7f78d28a8304d72dc63b6a290f3630c5793faf
Ruslan <ruslansolodovnyk@gmail.com>
2018-11-06 23:04:28 +0200

    add star projection

--- src/generics/building.kt
+++ src/generics/building.kt
@@ -9,6 +9,8 @@ class Brick : BaseBuildingMaterial() {
     override val numberNeeded = 8
 }
 
+inline fun <reified T: BaseBuildingMaterial> BaseBuildingMaterial.hasType() = this is T
+
 class Building<out T : BaseBuildingMaterial>(val baseBuildingMaterial: T) {
     val baseMaterialsNeeded: Int = 100
     val actualMaterialsNeed: Int = (baseMaterialsNeeded * baseBuildingMaterial.numberNeeded)
@@ -18,7 +20,7 @@ class Building<out T : BaseBuildingMaterial>(val baseBuildingMaterial: T) {
     }
 }
 
-inline fun <reified T: BaseBuildingMaterial> BaseBuildingMaterial.hasType() = this is T
+inline fun <reified R: BaseBuildingMaterial> Building<*>.hasBaseBuildingMaterial() = baseBuildingMaterial is R
 
 fun <T : BaseBuildingMaterial> isSmallBuilding(building: Building<T>) {
     if (building.actualMaterialsNeed < 500) {
@@ -36,5 +38,6 @@ fun main(args: Array<String>) {
     val building = Building(Wood())
     building.build()
     println(building.baseBuildingMaterial.hasType<Wood>())
+    println(building.hasBaseBuildingMaterial<Brick>())
     isSmallBuilding(building)
 }
