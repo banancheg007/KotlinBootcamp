c8199f312221ade2ec24eb7cc230110ca7a2a404
Ruslan <ruslansolodovnyk@gmail.com>
2018-11-06 22:57:12 +0200

    Use generic in and out types

--- src/generics/building.kt
+++ src/generics/building.kt
@@ -8,14 +8,22 @@ class Wood : BaseBuildingMaterial() {
 class Brick : BaseBuildingMaterial() {
     override val numberNeeded = 8
 }
-class Building<T : BaseBuildingMaterial>(val baseBuildingMaterial: T) {
+
+class Building<out T : BaseBuildingMaterial>(val baseBuildingMaterial: T) {
     val baseMaterialsNeeded: Int = 100
     val actualMaterialsNeed: Int = (baseMaterialsNeeded * baseBuildingMaterial.numberNeeded)
+
     fun build() {
         println("$actualMaterialsNeed ${baseBuildingMaterial::class.simpleName} required")
     }
 }
+
+fun addItem(building: Building<BaseBuildingMaterial>) {
+    println("item added")
+}
+
 fun main(args: Array<String>) {
     val building = Building(Wood())
     building.build()
+    addItem(building)
 }
